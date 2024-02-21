7d94fbe6b5821fd8f8461dad79af853b2941ff92
R Aswin <aswinr242004@gmail.com>
2024-02-21 15:05:04 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Main from './home/Main'
+import Navbar from './home/Navbar'
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Main />
+    </>
+  )
+}
+
+export default App
